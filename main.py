7835e3b55d0ee7c314404dd7d33563133f813693
AJ-SM <anujsidam1108@gmail.com>
2026-07-26 22:34:05 +0530

    added the chunking

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import os
 from uitls import read_fileTxt
 from uitls import preProcess
-
+from uitls import Chunk
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Getting the file
-file_path = r"D:\Storeage-1\MlNewBorn\RRAG\res\data.txt"
+file_path =os.getenv('DPATH')
+
 # Reading the whole file
 data = read_fileTxt(file_path)
 # Creating a single string NOT recommended for very big txt Python usually have the limit of  63 GB
 txt = preProcess(data)
+chuz = Chunk(txt)
+print(chuz)
 
 
 
--- uitls.py
+++ uitls.py
@@ -1,19 +1,54 @@
 import os
+from dotenv import load_dotenv
+import os
+load_dotenv()
+filePath = os.getenv('DPATH')
 
-
-filePath= r"D:\Storeage-1\MlNewBorn\RRAG\res\data.txt"
-
+# Read and get Functions DATA..
 def read_fileTxt(filepath:str):
     with open(filepath,'r') as f:
         return f.readlines()
 
 def preProcess(data:list):
-    final_txt= ""
+    final_txt=""
     for line in data:
-        final_txt += line[:-2]
+        ls =  line[:-1]
+        ls = ls.strip()
+
+        if not ls:
+            continue
+        if not ls.endswith('.'):
+            ls+="."
+
+        final_txt+=ls
     return final_txt
+
+
+# Do Chunking
+def Chunk(data:list, chunkSize:int = 500):
+    chunks=[]
+    target = ""
+    ctr= 0
+    for word in data:
+
+        if ctr==chunkSize :
+            chunks.append(target)
+            target = ""
+            ctr=0
+        else:
+            target+=word
+            ctr+=1
+
+    chunks.append(target)
+
+
+
+
+
+    return chunks
 
 if __name__=='__main__':
     content = read_fileTxt(filePath)
     txt = preProcess(content)
-    print(txt)
+    # print(txt)
+    ckz =Chunk(txt)
